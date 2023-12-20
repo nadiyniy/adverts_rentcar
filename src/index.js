4ef3e3ce4a24d4abee1e76e075774ee4aeabae13
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-20 18:24:07 +0200

    delete homepage with package.json (go to vercel)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/adverts_rentcar">
+  <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
